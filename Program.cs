cb9d75b7d2f71c3f6df7c73ef52c21b76b17b370
James Enmanuel Urena Paulino <james_urena@ucne.edu.do>
2025-09-02 21:38:18 -0400

    Inyectando el Contexto

--- Program.cs
+++ Program.cs
@@ -1,18 +1,22 @@
+using Microsoft.EntityFrameworkCore;
 using RegistroJugadores.Components;
+using RegistroJugadores.DAL;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+var ConStr = builder.Configuration.GetConnectionString("SqlConStr");
+builder.Services.AddDbContextFactory<Contexto>(o => o.UseSqlServer(ConStr));
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
