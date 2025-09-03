9e0420ba75949a92c5ba2742fc0332720187a85c
James Enmanuel Urena Paulino <james_urena@ucne.edu.do>
2025-09-02 21:11:02 -0400

    Agregando el modelo

---
+++ Models/Jugadores.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistroJugadores.Models
+{
+    public class Jugadores
+    {
+        [Key]
+        public int JugadorId { get; set; }
+
+        [Required(ErrorMessage = "El campo 'Nombre' es obligario.")]
+        public string Nombres { get; set; }
+
+        [Required(ErrorMessage = "El campo 'Partidas' es obligatorio.")]
+        [Range(0, int.MaxValue, ErrorMessage = "El numero de partida debe ser un valor valido.")]
+        public int Partidas { get; set; }
+    }
+}
