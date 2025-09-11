f4b2a676f5ecf269006f2c7c853fbe97a17bb0fd
James Enmanuel Urena Paulino <james_urena@ucne.edu.do>
2025-09-10 23:28:48 -0400

    Agregando Validaciones al Modelo

---
+++ Models/Partidas.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistroJugadores.Models
+{
+    public class Partidas
+    {
+        [Key]
+        public int PartidasId { get; set; }
+        [Range(1, int.MaxValue, ErrorMessage = "Debe Seleccionar un Jugador Valido")]
+        public int JugadorId { get; set; }
+        public int? Jugador2Id { get; set; }
+        [Required(ErrorMessage = " Debe Espeficicar el Estado ")]
+
+
+    }
+}
