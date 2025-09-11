2cff527e520d1b0ab51dcfe4aa01c14aa5e71611
James Enmanuel Urena Paulino <james_urena@ucne.edu.do>
2025-09-11 00:02:13 -0400

    Terminanado las validaciones del Modelo

--- Models/Partidas.cs
+++ Models/Partidas.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace RegistroJugadores.Models
 {
@@ -7,10 +8,32 @@ namespace RegistroJugadores.Models
         [Key]
         public int PartidasId { get; set; }
         [Range(1, int.MaxValue, ErrorMessage = "Debe Seleccionar un Jugador Valido")]
-        public int JugadorId { get; set; }
+        public int Jugador1Id { get; set; }
         public int? Jugador2Id { get; set; }
-        [Required(ErrorMessage = " Debe Espeficicar el Estado ")]
+        [Required(ErrorMessage = " Debe Espeficicar el estado de la Partida")]
+        [StringLength(20)]
+        public string EstadoPartida { get; set; } = null!;
+        public int? GanadorId { get; set; }
+        [Range(1, int.MaxValue, ErrorMessage = "'Debe especificar el turno de un jugador Valido")]
+        public int TurnoJugadorId { get; set; }
+        public DateTime FechaInicio { get; set; } = DateTime.Now;
+        public DateTime? FechaFin { get; set; }
 
+        [ForeignKey("Jugador1Id")]
+        [InverseProperty("PartidasJugador1")]
+        public virtual Jugadores Jugador1 { get; set; } = null!;
+
+        [ForeignKey("Jugador2Id")]
+        [InverseProperty("PartidasJugador2")]
+        public virtual Jugadores? Jugador2 { get; set; }
+
+        [ForeignKey("GanadorId")]
+        [InverseProperty("PartidasGanadas")]
+        public virtual Jugadores? Ganador { get; set; }
+
+        [ForeignKey("TurnoJugadorId")]
+        [InverseProperty("PartidasTurno")]
+        public virtual Jugadores TurnoJugador { get; set; } = null!;
 
     }
 }
