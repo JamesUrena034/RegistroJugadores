2d0f0fd7a87c8fb85b1a8a34f2552c178b167c1c
James Enmanuel Urena Paulino <james_urena@ucne.edu.do>
2025-09-11 00:39:32 -0400

    Modificando el contexto

--- DAL/Contexto.cs
+++ DAL/Contexto.cs
@@ -8,5 +8,34 @@ namespace RegistroJugadores.DAL
         public Contexto(DbContextOptions<Contexto> options) : base(options) { }
 
         public DbSet<Jugadores> Jugadores { get; set; }
+        public DbSet<Partidas> Partidas { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Partidas>(entity =>
+            {
+                entity.HasOne(p => p.Jugador1)
+                      .WithMany()
+                      .HasForeignKey(p => p.Jugador1Id)
+                      .OnDelete(DeleteBehavior.Restrict);
+
+                entity.HasOne(p => p.Jugador2)
+                      .WithMany()
+                      .HasForeignKey(p => p.Jugador2Id)
+                      .OnDelete(DeleteBehavior.Restrict);
+
+                entity.HasOne(p => p.Ganador)
+                      .WithMany()
+                      .HasForeignKey(p => p.GanadorId)
+                      .OnDelete(DeleteBehavior.Restrict);
+
+                entity.HasOne(p => p.TurnoJugador)
+                      .WithMany()
+                      .HasForeignKey(p => p.TurnoJugadorId)
+                      .OnDelete(DeleteBehavior.Restrict);
+            });
+        }
+
     }
 }
+
