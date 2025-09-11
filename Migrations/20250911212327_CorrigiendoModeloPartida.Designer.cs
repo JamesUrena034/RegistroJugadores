1a43ad3d591c0b58da0f712bf8fcc383975c3e56
James Enmanuel Urena Paulino <james_urena@ucne.edu.do>
2025-09-11 18:41:23 -0400

    Corrigiendo el modelo de partidas

--- Migrations/20250911212327_CorrigiendoModeloPartida.Designer.cs
+++ Migrations/20250911212327_CorrigiendoModeloPartida.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RegistroJugadores.DAL;
 
@@ -11,9 +12,11 @@ using RegistroJugadores.DAL;
 namespace RegistroJugadores.Migrations
 {
     [DbContext(typeof(Contexto))]
-    partial class ContextoModelSnapshot : ModelSnapshot
+    [Migration("20250911212327_CorrigiendoModeloPartida")]
+    partial class CorrigiendoModeloPartida
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -49,11 +52,6 @@ namespace RegistroJugadores.Migrations
                         .HasColumnType("int");
 
                     SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("PartidasId"));
-
-                    b.Property<string>("EstadoPartida")
-                        .IsRequired()
-                        .HasMaxLength(20)
-                        .HasColumnType("nvarchar(20)");
 
                     b.Property<DateTime?>("FechaFin")
                         .HasColumnType("datetime2");
---
+++ Migrations/20250911212327_CorrigiendoModeloPartida.cs
@@ -0,0 +1,150 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RegistroJugadores.Migrations
+{
+    /// <inheritdoc />
+    public partial class CorrigiendoModeloPartida : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Partidas_Jugadores_JugadoresJugadorId",
+                table: "Partidas");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Partidas_Jugadores_JugadoresJugadorId1",
+                table: "Partidas");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Partidas_Jugadores_JugadoresJugadorId2",
+                table: "Partidas");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Partidas_Jugadores_JugadoresJugadorId3",
+                table: "Partidas");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Partidas_JugadoresJugadorId",
+                table: "Partidas");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Partidas_JugadoresJugadorId1",
+                table: "Partidas");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Partidas_JugadoresJugadorId2",
+                table: "Partidas");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Partidas_JugadoresJugadorId3",
+                table: "Partidas");
+
+            migrationBuilder.DropColumn(
+                name: "EstadoPartida",
+                table: "Partidas");
+
+            migrationBuilder.DropColumn(
+                name: "JugadoresJugadorId",
+                table: "Partidas");
+
+            migrationBuilder.DropColumn(
+                name: "JugadoresJugadorId1",
+                table: "Partidas");
+
+            migrationBuilder.DropColumn(
+                name: "JugadoresJugadorId2",
+                table: "Partidas");
+
+            migrationBuilder.DropColumn(
+                name: "JugadoresJugadorId3",
+                table: "Partidas");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "EstadoPartida",
+                table: "Partidas",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "JugadoresJugadorId",
+                table: "Partidas",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "JugadoresJugadorId1",
+                table: "Partidas",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "JugadoresJugadorId2",
+                table: "Partidas",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "JugadoresJugadorId3",
+                table: "Partidas",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Partidas_JugadoresJugadorId",
+                table: "Partidas",
+                column: "JugadoresJugadorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Partidas_JugadoresJugadorId1",
+                table: "Partidas",
+                column: "JugadoresJugadorId1");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Partidas_JugadoresJugadorId2",
+                table: "Partidas",
+                column: "JugadoresJugadorId2");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Partidas_JugadoresJugadorId3",
+                table: "Partidas",
+                column: "JugadoresJugadorId3");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Partidas_Jugadores_JugadoresJugadorId",
+                table: "Partidas",
+                column: "JugadoresJugadorId",
+                principalTable: "Jugadores",
+                principalColumn: "JugadorId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Partidas_Jugadores_JugadoresJugadorId1",
+                table: "Partidas",
+                column: "JugadoresJugadorId1",
+                principalTable: "Jugadores",
+                principalColumn: "JugadorId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Partidas_Jugadores_JugadoresJugadorId2",
+                table: "Partidas",
+                column: "JugadoresJugadorId2",
+                principalTable: "Jugadores",
+                principalColumn: "JugadorId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Partidas_Jugadores_JugadoresJugadorId3",
+                table: "Partidas",
+                column: "JugadoresJugadorId3",
+                principalTable: "Jugadores",
+                principalColumn: "JugadorId");
+        }
+    }
+}
