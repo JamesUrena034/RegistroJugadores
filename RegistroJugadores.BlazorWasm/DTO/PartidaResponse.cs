e3ae9dab186396be02122e8930d8d70c732ed7fc
James Enmanuel Urena Paulino <james_urena@ucne.edu.do>
2025-10-25 17:09:46 -0400

    Realizando el Dto PartidaResponse

---
+++ RegistroJugadores.BlazorWasm/DTO/PartidaResponse.cs
@@ -0,0 +1,8 @@
+﻿    namespace RegistroJugadores.BlazorWasm.Models.ApiDtos;
+
+public class PartidaResponse
+{
+    public int PartidaId { get; set; }
+    public int Jugador1Id { get; set; }
+    public int? Jugador2Id { get; set; }
+}
