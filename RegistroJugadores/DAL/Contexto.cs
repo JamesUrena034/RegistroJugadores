10a205bcd42d0c4a816269e08c42f36510b0f615
James Enmanuel Urena Paulino <james_urena@ucne.edu.do>
2025-10-25 17:04:02 -0400

    Creando nueva Carpeta RegistroJugadores

---
+++ RegistroJugadores/DAL/Contexto.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RegistroJugadores.Models;
+
+namespace RegistroJugadores.DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Jugadores> Jugadores { get; set; }
+        public DbSet<Partidas> Partidas { get; set; }
+        public DbSet<Movimientos> Movimientos { get; set; }
+
+        public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Partidas>(entity =>
+            {
+                entity.HasOne(p => p.Jugador1)
+                      .WithMany()
+                      .HasForeignKey(p => p.Jugador1Id)
+                      .OnDelete(DeleteBehavior.Restrict);
+
+                entity.HasOne(p => p.Jugador2)
+                      .WithMany()
+                      .HasForeignKey(p => p.Jugador2Id)
+                      .OnDelete(DeleteBehavior.Restrict);
+
+                entity.HasOne(p => p.Ganador)
+                      .WithMany()
+                      .HasForeignKey(p => p.GanadorId)
+                      .OnDelete(DeleteBehavior.Restrict);
+
+                entity.HasOne(p => p.TurnoJugador)
+                      .WithMany()
+                      .HasForeignKey(p => p.TurnoJugadorId)
+                      .OnDelete(DeleteBehavior.Restrict);
+            });
+        }
+    }
+}
---
+++ RegistroJugadores/Models/Jugadores.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RegistroJugadores.Models;
+
+public class Jugadores
+{
+    [Key]
+    public int JugadorId { get; set; }
+
+    [Required(ErrorMessage = "El campo 'Nombres' es obligatorio.")]
+    public string Nombres { get; set; } = null!;
+
+    public int Victorias { get; set; } = 0;
+    public int Derrotas { get; set; } = 0;
+    public int Empates { get; set; } = 0;
+
+    [InverseProperty(nameof(Models.Movimientos.Jugadores))]
+    public virtual ICollection<Movimientos> Movimientos { get; set; } = new List<Movimientos>();
+
+}
