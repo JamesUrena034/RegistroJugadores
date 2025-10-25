45917cff58ae1b66db9a648da3b69e25c4903e85
James Enmanuel Urena Paulino <james_urena@ucne.edu.do>
2025-10-25 17:06:19 -0400

    Realizando el Dto  MovimientoResponse

---
+++ RegistroJugadores.BlazorWasm/DTO/MovimientosResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace RegistroJugadores.BlazorWasm.Models.ApiDtos;
+
+public class MovimientosResponse
+{
+    public int MovimientoId { get; set; }
+    public string Jugador { get; set; } = "X";
+
+    public int PosicionFila { get; set; }
+
+    public int PosicionColumna { get; set; }
+}
